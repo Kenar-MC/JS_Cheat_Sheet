489a9174bd8beca233f9bbcae1dda3fb9ccee5c5
Kenar-MC <kenar.fresh@gmail.com>
2024-11-26 00:18:25 +0300

    Add operators cheat sheet with examples for arithmetic, assignment, and comparison operators

---
+++ operators.js
@@ -0,0 +1,38 @@
+//Operators Cheat Sheet
+
+    // Arithmetic Operators
+
+        let addition = 1 + 1;
+        let subtraction = 1 - 1;
+        let multiplication = 1 * 1;
+        let division = 1 / 1;
+        let remainder = 5 % 2;
+        
+        let increment = 1;
+        increment++;    // after the variable
+        ++increment;    // before the variable
+        
+        let decrement = 1;
+        decrement--;    // after the variable
+        --decrement;    // before the variable
+
+    // Assignment Operators
+
+        let x = 1; // Assigns the value 1 to x
+        x += 1; // Adds 1 to x
+        x -= 1; // Subtracts 1 from x
+        x *= 1; // Multiplies x by 1
+        x /= 1; // Divides x by 1
+        x %= 1; // Assigns the remainder of dividing x by 1 to x
+        
+    // Comparison Operators
+
+        let equal = 1 == 1;
+        console.log(equal); // true
+        let notEqual = 1 != 1;
+        console.log(notEqual); // false
+        
+        let numGreaterThan = 2 > 1;
+        console.log(numGreaterThan); // true 
+        let numLessThan = 1 < 2;
+        console.log(numLessThan); // false

907bdbde54dc2a16aed64f508c7d83c347097197
Kenar-MC <kenar.fresh@gmail.com>
2024-11-26 01:03:34 +0300

    Add control flow example to control-flow.js with basic buying logic

--- control-flow.js
+++ control-flow.js
@@ -1,1 +1,9 @@
-// 
+//Control Flow Cheat Sheet
+
+    // Basic
+    let price = 100;
+    let money = 100;
+
+    const buy = (price, money) >= price ? "You can buy it!" : "You don't have enough money!";
+
+    

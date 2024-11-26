97a35901936023399e53d4f03dfc7f5c9df6a866
Kenar-MC <kenar.fresh@gmail.com>
2024-11-26 09:46:06 +0300

    Refactor control-flow.js and operators.js for consistent indentation and formatting

--- control-flow.js
+++ control-flow.js
@@ -1,41 +1,57 @@
 //Control Flow Cheat Sheet
 
     // Basic
-    let price = 110;
-    let money = 100;
 
-    const canBuy = money >= price;
+                let price = 110;
+                let money = 100;
+
+                const canBuy = money >= price;
 
     // If Statement
 
-    Job = "Web Developer";
+                Job = "Web Developer";
 
-    if (Job === "Web Developer")
-    {
-        console.log("I am a Web Developer");
-    }
-    else if (Job === "Designer")
-    {
-        console.log("I am a Designer");
-    }
-    else
-    {
-        console.log("I am a Student");
-    }
+                if (Job === "Web Developer")
+                {
+                    console.log("I am a Web Developer");
+                }
+                else if (Job === "Designer")
+                {
+                    console.log("I am a Designer");
+                }
+                else
+                {
+                    console.log("I am a Student");
+                }
 
     // Switch Statement
 
-    Job2 = "Designer";
+                Job2 = "Designer";
 
-    switch (Job2)
-    {
-        case "Web Developer":
-            console.log("I am a Web Developer");
-            break;
-        case "Designer":
-            console.log("I am a Designer");
-            break;
-        default:
-            console.log("I am a Student");
-    }
-    
+                switch (Job2)
+                {
+                    case "Web Developer":
+                        console.log("I am a Web Developer");
+                        break;
+                    case "Designer":
+                        console.log("I am a Designer");
+                        break;
+                    default:
+                        console.log("I am a Student");
+                }
+                
+    // For Loop
+
+                let numbers = [1, 2, 3, 4, 5];
+
+                //   let i = 0;
+                //   console.log(numbers[i]);      1
+                //   console.log(numbers[i++]);    2
+                //   console.log(numbers[i++]);    3
+                //   console.log(numbers[i++]);    4
+                //   console.log(numbers[i++]);    5
+                
+                for (let i = 0; i < numbers.length; i++)
+                {
+                    console.log(numbers[i]);
+                }
--- operators.js
+++ operators.js
@@ -1,81 +1,91 @@
 //Operators Cheat Sheet
 
     // Arithmetic Operators
-        let addition = 1 + 1;
-        let subtraction = 1 - 1;
-        let multiplication = 1 * 1;
-        let division = 1 / 1;
-        let remainder = 5 % 2;
-        
-        let increment = 1;
-        increment++;    // after the variable
-        ++increment;    // before the variable
-        
-        let decrement = 1;
-        decrement--;    // after the variable
-        --decrement;    // before the variable
+
+                let addition = 1 + 1;
+                let subtraction = 1 - 1;
+                let multiplication = 1 * 1;
+                let division = 1 / 1;
+                let remainder = 5 % 2;
+                
+                let increment = 1;
+                increment++;    // after the variable
+                ++increment;    // before the variable
+                
+                let decrement = 1;
+                decrement--;    // after the variable
+                --decrement;    // before the variable
 
     // Assignment Operators
-        let x = 1; // Assigns the value 1 to x
-        x += 1; // Adds 1 to x
-        x -= 1; // Subtracts 1 from x
-        x *= 1; // Multiplies x by 1
-        x /= 1; // Divides x by 1
-        x %= 1; // Assigns the remainder of dividing x by 1 to x
-        
-    // Comparison Operators       
-        let numGreaterThan = 2 > 1; // true
-        let numLessThan = 1 < 2;    // true
 
-        let greaterThanOrEqual = 1 >= 1; // true
-        let lessThanOrEqual = 1 <= 1;    // true
+                let x = 1; // Assigns the value 1 to x
+                x += 1; // Adds 1 to x
+                x -= 1; // Subtracts 1 from x
+                x *= 1; // Multiplies x by 1
+                x /= 1; // Divides x by 1
+                x %= 1; // Assigns the remainder of dividing x by 1 to x
+                
+    // Comparison Operators   
+
+                let numGreaterThan = 2 > 1; // true
+                let numLessThan = 1 < 2;    // true
+
+                let greaterThanOrEqual = 1 >= 1; // true
+                let lessThanOrEqual = 1 <= 1;    // true
 
     // Equality Operators
 
-        // Truthy
-        let equal = 1 == 1; // true
-       
-        // Strict Equality       
-        let strictEqual = 1 === 1; // true
-        let notStrictEqual = 1 !== 1; // false
+                // Truthy
+
+                    let equal = 1 == 1; // true
+            
+                // Strict Equality  
+
+                    let strictEqual = 1 === 1; // true
+                    let notStrictEqual = 1 !== 1; // false
 
     // Ternary Operator
-        let age = 18;
-        let canVote = (age >= 18) ? "Yes" : "No"; // Yes
 
-        let points = 90;
-        let type = (points > 100) ? "Gold" : (points > 90) ? "Silver" : "Bronze"; // Silver
+                let age = 18;
+                let canVote = (age >= 18) ? "Yes" : "No"; // Yes
+
+                let points = 90;
+                let type = (points > 100) ? "Gold" : (points > 90) ? "Silver" : "Bronze"; // Silver
 
     // Logical Operators
-        let andVariable = true && true; // true
-        let orVariable = true || false; // true
-        let notVariable = !true; // false
+
+                let andVariable = true && true; // true
+                let orVariable = true || false; // true
+                let notVariable = !true; // false
         
-            // Ternary and Logical Operator Mix
-             let personAge = 18;
-             let hasCar = true;
-             let canDrive = personAge >= 16 && hasCar; // true
+                // Ternary and Logical Operator Mix
 
-        let a = null;
-        const result1 = a ?? "default value"; // "default value"
-        const result2 = (a !== null && a !== undefined) ? a : "default value"; // "default value"
+                    let personAge = 18;
+                    let hasCar = true;
+                    let canDrive = personAge >= 16 && hasCar; // true
 
-        const result3 = a || "default value"; // "default value"
-        const result4 = (a !== null && a !== undefined) ? a : "default value"; // "default value"
+                    let a = null;
+                    const result1 = a ?? "default value"; // "default value"
+                    const result2 = (a !== null && a !== undefined) ? a : "default value"; // "default value"
+
+                    const result3 = a || "default value"; // "default value"
+                    const result4 = (a !== null && a !== undefined) ? a : "default value"; // "default value"
     
     // Logical Operators with Non-Boolean Values
-        false || `Hello`; // "Hello"
-        false || 1; // 1
-        
-        // Falsy values are short-circuited
-        false || 1 || 2; // 1
-        false || 0 || 2; // 2
-        false || undefined || 2; // 2
-        false || null || 2; // 2
-        false || "" || 2; // 2
-        
-        // Truthy values are short-circuited
-        true || 1; // true
-        true || `Hello`; // true
+
+                false || `Hello`; // "Hello"
+                false || 1; // 1
+                
+                // Falsy values are short-circuited
+                false || 1 || 2; // 1
+                false || 0 || 2; // 2
+                false || undefined || 2; // 2
+                false || null || 2; // 2
+                false || "" || 2; // 2
+                
+                // Truthy values are short-circuited
+
+                    true || 1; // true
+                    true || `Hello`; // true
         
         

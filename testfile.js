d5da66583625c35e10a378d21c6358e436408d6d
Kenar-MC <kenar.fresh@gmail.com>
2024-11-26 12:08:31 +0300

    Replace test function with fizzbuzz implementation in testfile.js

--- testfile.js
+++ testfile.js
@@ -1,6 +1,15 @@
-function test(a, b) {
-    return a > b ? a : b;
+// function test(a, b) {
+//     return a > b ? a : b;
+// }
+
+// console.log(test(5, 10));
+
+function fizzbuzz (n)
+{
+    if (n % 15 == 0) return 'fizzbuzz';
+    if (n % 3 == 0) return 'fizz';
+    if (n % 5 == 0) return 'buzz';
+    return n;
 }
 
-console.log(test(5, 10));
-
+console.log(fizzbuzz(15));

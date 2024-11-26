e525e463370913e4b1b312413b58faa2ab4819a5
Kenar-MC <kenar.fresh@gmail.com>
2024-11-26 16:04:42 +0300

    Add objets.js with object creation examples and methods

---
+++ objets.js
@@ -0,0 +1,34 @@
+// Objects Cheat Sheet
+
+    // Object Creation
+
+                let person = 
+                {
+                    name: "John",                   // Property / Key
+                    age: 30,
+                    city: "New York",
+
+                    eat : function() {              // Behaviour / Method
+                        console.log("Eating");
+                    },
+                    function() {
+                        console.log("Sleeping");
+                    }
+                };
+    
+                let anotherPerson = 
+                {
+                    name: "Jane",
+                    age: 25,
+                    city: "San Francisco",
+
+                    eat : function() {
+                        console.log("Not Eating");
+                    },
+
+                    function() {
+                        console.log("Sleeping");
+                    }
+                }
+    
+

bcea7eefabe1a315337ee5591f514f435d957e5c
Kenar-MC <kenar.fresh@gmail.com>
2024-12-03 19:56:31 +0300

    Enhance arrays.js with additional examples for array manipulation methods and improve clarity

--- arrays.js
+++ arrays.js
@@ -1,16 +1,34 @@
 // Arrays Cheat Sheet
 
-                const numbers = [1, 2, 3, 4, 5]; // Array of numbers
+                let numbers = [1, 2, 3, 4, 5]; // Array of numbers
+                console.log(numbers); // [1, 2, 3, 4, 5]
 
                 numbers.push(6); // Add to the end of the array // [1, 2, 3, 4, 5, 6]
                 numbers.unshift(0); // Add to the beginning of the array // [0, 1, 2, 3, 4, 5, 6]
                 numbers.splice(2, 0, 1.5); // Add to the middle of the array // [0, 1, 1.5, 2, 3, 4, 5, 6]
+                numbers.pop(); // Remove from the end of the array // [0, 1, 1.5, 2, 3, 4, 5]
+                numbers.shift(); // Remove from the beginning of the array // [1, 1.5, 2, 3, 4, 5]
+                numbers.splice(2, 1); // Remove from the middle of the array // [1, 1.5, 3, 4, 5]
 
                 numbers.indexOf(3); // 4 // Find the index of an element
                 numbers.lastIndexOf(3); // 4 // Find the last index of an element
 
                 numbers.includes(3); // true // Check if an element exists in an array
                 numbers.includes(10); // false
+
+
+                while (numbers.length > 0) 
+                {
+                    console.log(numbers.pop());     // Removes all elements from the array
+                }
+
+                numbers.length = 0; // Removes all elements from the array
+
+                numbers.splice(0, numbers.length); // Removes all elements from the array
+
+                numbers = []; // Removes all elements from the array
+                
+                console.log(numbers); // []
 
 
                 const employees = 
@@ -20,10 +38,13 @@
                     {id: 3, name: "Smith"}
                 ]
 
-                console.log(employees.find(function(employee){return employee.name === "Doe"})) // {id: 2, name: "Doe"}
+                     
+                console.log(employees.findIndex(function(employee){return employee.name === "Doe"})) // 1  // Find the index of an element
+
+
+                console.log(employees.find(function(employee){return employee.name === "Doe"})) // {id: 2, name: "Doe"}  // Find an element
                
 
-                console.log(employees.find(employee => employee.name === "Doe")); // {id: 2, name: "Doe"}
+                console.log(employees.find(employee => employee.name === "Doe")); // {id: 2, name: "Doe"}  // Find an element but with arrow function
 
-                
-                console.log(employees.findIndex(function(employee){return employee.name === "Doe"})) // 1
+           

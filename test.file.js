f54f350c221927032299526b54194b485a0bf5e5
Kenar-MC <kenar.fresh@gmail.com>
2025-01-14 03:19:43 +0300

    test

--- test.file.js
+++ test.file.js
@@ -1,8 +1,22 @@
 
-let input = "asd dsa"
-let a = input.indexOf(" ")
-console.log(a) 
+// let input = "asd dsa"
+// let a = input.indexOf(" ")
+// console.log(a) 
 
-let str = 'JavaScript';
-let newstr = str.replace('Java', 'ECMA');
-console.log(newstr);
+// let str = 'JavaScript';
+// let newstr = str.replace('Java', 'ECMA');
+// console.log(newstr);
+
+
+let teams = [
+    ['Ruby', 'Angel'],
+    ['Mia', 'Yui', 'Merve', 'Elif'],
+    ['Saanvi', 'Giulia', 'Zeynep']
+  ];
+  
+  let row1 = [4, 9, 2];
+  let row2 = [3, 5, 7];
+  let row3 = [8, 1, 6];
+  let loshu = [row1, row2, row3];
+
+  console.log(teams);
